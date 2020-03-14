8c068802f44c8757beefa7619e5c58d1e16a5e86
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-03-14 11:46:04 -0300

    Adicionando constante de multiplicador

--- src/main/java/model/Constantes.java
+++ src/main/java/model/Constantes.java
@@ -4,5 +4,14 @@ public class Constantes {
 	static final String ENCODING_TYPE = "UTF-8";
 	static final String ENCRYPT_TYPE = "MD5";
 	static final String STRING_FORMAT = "%02X";
+	static int multiplicador = 1;
+	
+	public static int getMultiplicador() {
+		return multiplicador;
+	}
+	
+	public static void increaseMultiplicador() {
+		multiplicador += 2;
+	}
 
 }
